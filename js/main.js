5a5901d4357a5c70a9a41209bc4a3f41a4283554
burakcaglar <burakcaglar@yandex.com>
2020-11-07 00:12:06 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.addEventListener('keydown', function(event){
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+    
+    /*
+
+    setTimeout(function() {
+        key.classList.remove("playing");
+    }, 100);
+
+    */
+
+});
+
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
